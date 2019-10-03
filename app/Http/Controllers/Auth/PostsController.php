219c7bd78e61548545ec18e73a15ebfb4e2acc4f
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2019-10-03 10:48:54 -0700

    adding in PostsController

---
+++ app/Http/Controllers/Auth/PostsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostsController
+{
+    public function show()
+    {
+        return 'Hello';
+    }
+    
+    public function update()
+    {
+        return 'Hello';
+    }
+}
